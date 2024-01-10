052d2bbac328dffb1d42da30bee6fca662ab408b
AnhHocBE <quocanhle112@gmail.com>
2024-01-10 21:28:26 +0700

    App bar #1

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { Avatar, Divider, IconButton, ListItemIcon, Tooltip } from '@mui/material'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Box from '@mui/material/Box'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-meunu-profiles' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://img.freepik.com/premium-photo/cute-asian-girl-kawaii-anime-avatar-ai-generative-art_225753-9233.jpg'
+          >
+          </Avatar>
+        </IconButton>
+      </Tooltip>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profiles'
+        }}
+      >
+        <MenuItem>
+          <Avatar sx={{ width: '28px', height:'28px', mr: 2 }}/> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar sx={{ width: '28px', height:'28px', mr: 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,10 +1,67 @@
-import { Box } from '@mui/material'
+import Typography from '@mui/material/Typography'
 import ModeSelect from '~/components/ModeSlect'
-
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/material/SvgIcon'
+import WorkSpaces from './Menus/WorkSpaces'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import Box from '@mui/material/Box'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Badge from '@mui/material/Badge'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profiles from './Menus/Profiles'
 function AppBar() {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: 'primary.light', height: (theme) => theme.trello.appBarHeight, width: '100%' }}>
-      <ModeSelect/>
+    <Box px={2}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: (theme) => theme.trello.appBarHeight, width: '100%'
+      }}>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }}/>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon
+            component={TrelloIcon}
+            inheritViewBox
+            sx={{ color: 'primary.main' }}
+          />
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }} >Trello</Typography>
+        </Box>
+        <WorkSpaces/>
+        <Recent/>
+        <Starred/>
+        <Templates/>
+
+        <Button variant="outlined">Create</Button>
+
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+        <ModeSelect/>
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }} >
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+
+        <Tooltip title="Help">
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }} >
+            <HelpOutlineIcon />
+          </Badge>
+        </Tooltip>
+
+        <Profiles/>
+      </Box>
+
     </Box>
   )
 }
